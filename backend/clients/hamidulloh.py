2439572f1e1d9ccf3698be372499a5d2e385673d
Avzalcik <shuxratovafzal763gmail.com>
2026-05-10 06:28:39 -0700

    Add Hamidulloh client config

---
+++ backend/clients/hamidulloh.py
@@ -0,0 +1,12 @@
+from ._base import ClientConfig
+
+config = ClientConfig(
+    slug="Hamidulloh",
+    name="Hamidulloh",
+    admin_phones=[
+        "971585816776",
+    ],
+    drive_root_id="1IhO3Gq6e9mNs7xWVqdmwdx_YUr81hFws",   # ID папки Google Drive
+    umar_contact="@Hamidulloh",
+    bot_character="",   # пустой = стандартный Tony
+)
